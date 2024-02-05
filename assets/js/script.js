95e59faea42a40277fc8a8adb6d1046934daa968
czvrlvl <leaguemasterpl@gmail.com>
2024-02-05 19:06:42 +0100

    polityka prywatności (?) test

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var modal = document.getElementById('privacyPolicyModal');
+    var acceptButton = document.getElementById('acceptButton');
+
+   
+    var privacyAccepted = localStorage.getItem('privacyAccepted');
+
+    
+    if (!privacyAccepted) {
+        modal.style.display = 'flex';
+        document.body.style.overflow = 'hidden'; 
+    }
+
+   
+    acceptButton.addEventListener('click', function () {
+       
+        modal.style.display = 'none';
+
+       
+        document.body.style.overflow = 'auto'; 
+
+        
+        localStorage.setItem('privacyAccepted', 'true');
+    });
+});

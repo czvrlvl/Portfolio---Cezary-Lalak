d58cd3671adf581780c1eab262d4fc4b50fbc09c
czvrlvl <leaguemasterpl@gmail.com>
2024-02-05 19:15:04 +0100

    nie wiem nawet

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function () {
     var modal = document.getElementById('privacyPolicyModal');
     var acceptButton = document.getElementById('acceptButton');
 
-    // Check if the user has previously accepted the privacy policy using cookies
-    var privacyAccepted = getCookie('privacyAccepted');
+    // Check if the user has previously accepted the privacy policy using hash
+    var privacyAccepted = window.location.hash === '#accepted';
 
     // Show the modal only if not previously accepted
     if (!privacyAccepted) {
@@ -19,27 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Enable the rest of the website content
         document.body.style.overflow = 'auto'; // Enable scrolling
 
-        // Set a flag in cookie indicating privacy policy acceptance
-        setCookie('privacyAccepted', 'true', 365); // Cookie expires in 365 days
+        // Set a flag in the hash indicating privacy policy acceptance
+        window.location.hash = 'accepted';
     });
-
-    // Function to get the value of a cookie
-    function getCookie(name) {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            if (cookie.indexOf(name + '=') === 0) {
-                return cookie.substring(name.length + 1, cookie.length);
-            }
-        }
-        return null;
-    }
-
-    // Function to set a cookie
-    function setCookie(name, value, days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        var expires = 'expires=' + date.toUTCString();
-        document.cookie = name + '=' + value + '; ' + expires + '; path=/';
-    }
 });

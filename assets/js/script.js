116ea3710d66e21f1e6a0c4d18fa7e49b996ec2f
czvrlvl <leaguemasterpl@gmail.com>
2024-02-07 01:46:04 +0100

    Revert "poprawa w js"
    This reverts commit a94ba6b5f37d79b7e56f7985f0457f136b9e2ddf.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Pobierz referencje do modalu, przycisku akceptacji i body
-    var modal = document.getElementById("privacyPolicyModal");
+    var modal = document.querySelector(".modal");
     var acceptButton = document.getElementById("acceptButton");
-    var body = document.body;
 
-    // Funkcja do pokazania modala i zablokowania scrolla
+    // Funkcja do pokazania modala
     function showModal() {
         modal.style.display = "flex";
-        body.style.overflow = "hidden";
     }
 
-    // Funkcja do ukrycia modala i odblokowania scrolla
+    // Funkcja do ukrycia modala
     function hideModal() {
         modal.style.display = "none";
-        body.style.overflow = "auto";
     }
 
     // Pokaż modal po załadowaniu strony
